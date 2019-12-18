6989d796bc46c1fb3240e8c648aa4312b9fd0bed
thomaswong25520 <thomaswong8321@gmail.com>
2019-12-18 15:36:02 +0100

    new URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -76,12 +76,6 @@ class NewVisitorTest(LiveServerTestCase):
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
-        # Edith wonders whether the site will remember her list. Then she shees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        self.fail('Finish the test!')
-        # She visits that URL - her to-do list is still there.
-
         # Satisfied, she goes back to sleep
 
     def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -106,7 +100,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Francis visits the home page. There is no sign of Edith's
         # list
         self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tab_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
 
